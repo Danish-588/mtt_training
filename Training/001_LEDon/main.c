a5a6c90aafaccdb89fa5446c232eb1884939aa9e
Tapia Danish <danish.tapia588@gmail.com>
2023-05-20 22:20:03 +0530

    LED Code Done

--- Training/001_LEDon/main.c
+++ Training/001_LEDon/main.c
@@ -1,9 +1,12 @@
-//insert include for tm4c file
+#include <tm4c123gh6pge.h>
+#include <stdint.h>
 
-/**
- * main.c
- */
 int main(void)
 {
-	return 0;
+    SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R0; // GPIO Run Mode Clock
+    GPIO_PORTA_DIR_R  = 8; // GPIO Output (PORTA)
+    GPIO_PORTA_DEN_R  = 8; // GPIO Digital Enable (PORTA)
+    GPIO_PORTA_DATA_R = 8; // GPIO Value on (PORTA)
+
+    return 0;
 }
