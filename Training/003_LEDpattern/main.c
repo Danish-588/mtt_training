32d4cd2eb0c93c4dbdff4225c4021cc6f2fd34e0
Tapia Danish <danish.tapia588@gmail.com>
2023-05-22 15:54:21 +0530

    LEDpattern Complete

--- Training/003_LEDpattern/main.c
+++ Training/003_LEDpattern/main.c
@@ -1,9 +1,15 @@
+#include <stdint.h>
+#include <tm4c123gh6pge.h>
 
-
-/**
- * main.c
- */
 int main(void)
 {
-	return 0;
+    SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R6;
+    GPIO_PORTG_DIR_R  = 255;
+    GPIO_PORTG_DEN_R  = 255;
+    int i=0,a=0;
+    while (1)
+    {
+        GPIO_PORTG_DATA_R = (1<<(i%8))|(1<<(7-(i++%8)));      //pattern
+        for(a = 0; a < 400000; a++){}                       //delay
+    }
 }
