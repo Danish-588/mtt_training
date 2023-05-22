3e51774a05b44c7355f9f523037f77d5dc025a92
Tapia Danish <danish.tapia588@gmail.com>
2023-05-22 15:53:53 +0530

    LEDon Complete

--- Training/001_LEDon/main.c
+++ Training/001_LEDon/main.c
@@ -4,9 +4,9 @@
 int main(void)
 {
     SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R6; // GPIO Run Mode Clock
-    GPIO_PORTA_DIR_R  = 85;                 // GPIO to Output (PORTG)
-    GPIO_PORTA_DEN_R  = 85;                 // GPIO Digital Enable (PORTG)
-    GPIO_PORTA_DATA_R = 85;                 // GPIO Value on (PORTG)
+    GPIO_PORTG_DIR_R  = 1;                 // GPIO to Output (PORTG)
+    GPIO_PORTG_DEN_R  = 1;                 // GPIO Digital Enable (PORTG)
+    GPIO_PORTG_DATA_R = 1;                 // GPIO Value on (PORTG)
 
     while(1){}
 }
