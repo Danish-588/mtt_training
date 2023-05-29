058c6a8b22a6abeda6a46db0b95ff22e0d39e4dd
Tapia Danish <danish.tapia588@gmail.com>
2023-05-30 01:56:46 +0530

    Hit Switch with 1 led work

---
+++ Training/004_HitSwitchLED/main.c
@@ -0,0 +1,16 @@
+#include <tm4c123gh6pge.h>
+#include <stdint.h>
+int main(void)
+{
+    SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R6;  // GPIO Run Mode Clock On (PORTG)
+    GPIO_PORTG_DIR_R  = 2;                   // GPIO Pin G0 to Input and G1 to Output
+    GPIO_PORTG_DEN_R  = 255;                 // GPIO digital Enable (PORTG)
+    GPIO_PORTG_PDR_R  = 1;
+    while(1)
+        {
+            if(GPIO_PORTG_DATA_R&1)
+                GPIO_PORTG_DATA_R|=2;
+            else
+                GPIO_PORTG_DATA_R = 0;
+        }
+}
