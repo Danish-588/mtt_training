ee3d4c89af293e923bbe3907cdd04a905002e760
Tapia Danish <danish.tapia588@gmail.com>
2023-06-29 16:40:48 +0530

    Updated with comments

--- Training/004_HitSwitchLED/main.c
+++ Training/004_HitSwitchLED/main.c
@@ -5,12 +5,12 @@ int main(void)
     SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R6;  // GPIO Run Mode Clock On (PORTG)
     GPIO_PORTG_DIR_R  = 2;                   // GPIO Pin G0 to Input and G1 to Output
     GPIO_PORTG_DEN_R  = 255;                 // GPIO digital Enable (PORTG)
-    GPIO_PORTG_PDR_R  = 1;
+    GPIO_PORTG_PUR_R  = 1;
     while(1)
         {
             if(GPIO_PORTG_DATA_R&1)
-                GPIO_PORTG_DATA_R|=2;
-            else
                 GPIO_PORTG_DATA_R = 0;
+            else
+                GPIO_PORTG_DATA_R|= 2;
         }
 }
--- Training/005_HitSwitchTwo/main.c
+++ Training/005_HitSwitchTwo/main.c
@@ -3,16 +3,16 @@
 int main(void)
 {
     SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R6;  // GPIO Run Mode Clock On (PORTG)
-    GPIO_PORTG_DIR_R  = 48;
+    GPIO_PORTG_DIR_R  = 48;                  // p 4&5 output
     GPIO_PORTG_DEN_R  = 255;                 // GPIO digital Enable (PORTG)
-    GPIO_PORTG_PUR_R  = 3;
+    GPIO_PORTG_PUR_R  = 3;                   // p 0&1
     while(1)
         {
-            if((GPIO_PORTG_DATA_R & 1) == 0)
-                GPIO_PORTG_DATA_R |=(1<<4);
-            else(GPIO_PORTG_DATA_R &=(0<<4));
-            if((GPIO_PORTG_DATA_R & 2) == 0)
-                GPIO_PORTG_DATA_R |=(1<<5);
-            else(GPIO_PORTG_DATA_R &=(0<<5));       //to do ternaru
+            if((GPIO_PORTG_DATA_R & 1) == 0)    // if p0 is low
+                GPIO_PORTG_DATA_R |=(1<<4);     // turn on led p4
+            else(GPIO_PORTG_DATA_R &=(0<<4));   // turn off led p4
+            if((GPIO_PORTG_DATA_R & 2) == 0)    // if p1 is low
+                GPIO_PORTG_DATA_R |=(1<<5);     // turn on led p5
+            else(GPIO_PORTG_DATA_R &=(0<<5));   // turn off led p5
         }
 }
