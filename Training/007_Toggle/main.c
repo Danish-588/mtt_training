fe5c7774b3ecf31ae970c11d9bc069c94d0733fb
Tapia Danish <danish.tapia588@gmail.com>
2023-07-01 23:51:05 +0530

    LED toggle using delay

--- Training/007_Toggle/main.c
+++ Training/007_Toggle/main.c
@@ -1,9 +1,24 @@
+#include <tm4c123gh6pge.h>
+#include <stdint.h>
 
+volatile int old_data=0,i=0;
 
-/**
- * main.c
- */
+void delay(int a){
+    for (i=0;i<a;i++){}
+}
+
 int main(void)
 {
-	return 0;
+    SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R6;
+    GPIO_PORTG_DIR_R  = 48;
+    GPIO_PORTG_DEN_R  = 255;
+    GPIO_PORTG_PUR_R  = 3;
+
+    while(1)
+    {
+        old_data = GPIO_PORTG_DATA_R;       // stores the current data value to be compared with the new value
+        delay(7000);                        // delay to debounce
+        GPIO_PORTG_DATA_R = GPIO_PORTG_DATA_R ^ ((GPIO_PORTG_DATA_R & (~old_data)) << 4);  //(GPIO_PORTG_DATA_R & (~old_data) displays bits switching from 1 to 0
+    }
+
 }
