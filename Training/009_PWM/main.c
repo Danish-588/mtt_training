c06a8a24573a7059959684e28a324595633f0206
Tapia Danish <danish.tapia588@gmail.com>
2023-07-02 17:56:16 +0530

    changes

--- Training/009_PWM/main.c
+++ Training/009_PWM/main.c
@@ -4,15 +4,24 @@
 
 int main(void)
 {
-    SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R6 | ;  // GPIO Run Mode Clock On (PORTG)
-    SYSCTL_RCC_USEPWMDIV;
+    //clock
+    SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R6;  // GPIO Run Mode Clock On (PORTG)
+    SYSCTL_RCGC0_R = 0x0010.0000;            // PWM Clock
+    SYSCTL_RCC_R = SYSCTL_RCC_USESYSDIV | SYSCTL_RCC_PWMDIV_2; // Configure the Run-Mode Clock Configuration (RCC) register in the System Control module to use the PWM divide (USEPWMDIV) and set the divider (PWMDIV) to divide by 2 (000).
+
+    //configuring
     GPIO_PORTG_DIR_R  = 0x04;
     GPIO_PORTG_DEN_R  = 0x04;  // configured by clearing the DEN bit in the GPIO Digital Enable (GPIODEN)
     GPIO_PORTG_AFSEL_R = 0X04; // The digital alternate hardware functions are enabled by setting the appropriate bit in the GPIO Alternate Function Select (GPIOAFSEL)
     GPIO_PORTG_PCTL_R = GPIO_PCTL_PG2_M1PWM0;  // and configuring the PMCx bit field in the GPIO Port Control (GPIOPCTL) register to the numeric encoding shown in the table below
     GPIO_PORTG_AMSEL = 0X4;
 
-    PWM0_0_LOAD_R = GPIO_PORTG_DATA_R;
+    PWM0_CTL_R = 0x00000000;
+    PWM0_0_GENA_R = 0x0000008C;
+    PWM0_0_GENB_R = 0x0000080C;
+
+    //setting
+    PWM0_0_LOAD_R = ;
 
 
     PWM0_ENABLE_R =
