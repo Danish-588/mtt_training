7bcbac2dec4a0617e00cd51fcfe049c75145ada0
Tapia Danish <danish.tapia588@gmail.com>
2023-07-02 17:18:04 +0530

    update

--- Training/009_PWM/main.c
+++ Training/009_PWM/main.c
@@ -5,11 +5,11 @@
 int main(void)
 {
     SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R6 | ;  // GPIO Run Mode Clock On (PORTG)
-
+    SYSCTL_RCC_USEPWMDIV;
     GPIO_PORTG_DIR_R  = 0x04;
     GPIO_PORTG_DEN_R  = 0x04;  // configured by clearing the DEN bit in the GPIO Digital Enable (GPIODEN)
     GPIO_PORTG_AFSEL_R = 0X04; // The digital alternate hardware functions are enabled by setting the appropriate bit in the GPIO Alternate Function Select (GPIOAFSEL)
-    GPIO_PORTG_PCTL_R = 0x05;  // and configuring the PMCx bit field in the GPIO Port Control (GPIOPCTL) register to the numeric encoding shown in the table below
+    GPIO_PORTG_PCTL_R = GPIO_PCTL_PG2_M1PWM0;  // and configuring the PMCx bit field in the GPIO Port Control (GPIOPCTL) register to the numeric encoding shown in the table below
     GPIO_PORTG_AMSEL = 0X4;
 
     PWM0_0_LOAD_R = GPIO_PORTG_DATA_R;
