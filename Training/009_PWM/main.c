bcb3f81b3cca3701aa2af29a85cfb421bac4eb9c
Tapia Danish <danish.tapia588@gmail.com>
2023-07-04 01:08:17 +0530

    ..

--- Training/009_PWM/main.c
+++ Training/009_PWM/main.c
@@ -2,26 +2,31 @@
 #include <tm4c123gh6pge.h>
 #include <80mhz.h>
 
+int i =0;
 
 int main(void)
 {
     System80Mhz();
     //clock
     SYSCTL_RCGCPWM_R |= 0x2;
-    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R6; //SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOG;
+    SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOG; //SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R6;
 
-    GPIO_PORTG_AFSEL_R = 0X04;
-    GPIO_PORTG_PCTL_R = GPIO_PCTL_PG2_M1PWM0;
+    for(i=0;i<1000;i++){}   //delay
+
+    GPIO_PORTG_AFSEL_R |= 0X04;
+    GPIO_PORTG_DEN_R |= 0X4;
+    GPIO_PORTG_DIR_R |= 0X4;
+    GPIO_PORTG_PCTL_R |= GPIO_PCTL_PG2_M1PWM0;
 
 
-    SYSCTL_RCC_R |= SYSCTL_RCC_USEPWMDIV;
-    SYSCTL_RCC_R &= (~0x000E0000);       // /2divider    clearing 3 bits for clock div
+//    SYSCTL_RCC_R |= SYSCTL_RCC_USEPWMDIV;
+//    SYSCTL_RCC_R &= (~0x000E0000);       // /2divider    clearing 3 bits for clock div
 
 
 
     //configuring for count down mode
     PWM1_0_CTL_R = 0; //clearing
-    PWM1_0_GENA_R = PWM_0_GENA_ACTCMPAD_ZERO|PWM_0_GENA_ACTLOAD_M;
+    PWM1_0_GENA_R |= PWM_0_GENA_ACTCMPAD_ZERO|PWM_0_GENA_ACTLOAD_M;
 
     //generating
     PWM1_0_LOAD_R = 1249;       //31.25*40 = 1250-1 1249
