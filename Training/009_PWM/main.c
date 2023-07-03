823a055cd536ba63caf6f83fc7007069aaad8ae6
Tapia Danish <danish.tapia588@gmail.com>
2023-07-03 15:26:06 +0530

    changed load and cmpa value

--- Training/009_PWM/main.c
+++ Training/009_PWM/main.c
@@ -15,7 +15,6 @@ int main(void)
     GPIO_PORTG_DEN_R  = 0x04;
     GPIO_PORTG_AFSEL_R = 0X04;
     GPIO_PORTG_PCTL_R = GPIO_PCTL_PG2_M1PWM0;
-    GPIO_PORTG_AMSEL_R = 0X4;
 
     //configuring for countdown mode
     PWM0_CTL_R = 0x00000000;
@@ -23,10 +22,13 @@ int main(void)
     PWM0_0_GENB_R = 0x0000080C;
 
     //generating
-    PWM0_0_LOAD_R = 0x7C; //for 80KHz time period=125 microseconds therefore 124
-    PWM0_0_CMPA_R = 0x3E; //for 50% duty cycle
-    PWM0_CTL_R = 0x00000001; //starting the timer in pwm0
+    PWM0_0_LOAD_R = 0x4E2;      //31.25*40 = 1250
+    PWM0_0_CMPA_R = 0x271;      //for 50% duty cycle
+    PWM0_CTL_R = 0x00000001;    //starting the timer in pwm0
     PWM0_ENABLE_R = 0x00000001; //enable outputs
+
+    //for 32KHz frequency, time period = 31.25 microseconds
+    //PWM clock is half of system clock = 40 Mhz
 
     while (1)
     {
