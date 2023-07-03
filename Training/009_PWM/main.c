2ed9809d67b99293e0a033410dac8089ae33e21d
Tapia Danish <danish.tapia588@gmail.com>
2023-07-03 12:31:49 +0530

    updated

--- Training/009_PWM/main.c
+++ Training/009_PWM/main.c
@@ -7,14 +7,15 @@ int main(void)
     //clock
     SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R6;
     SYSCTL_RCGC0_R = 0x100000;            // PWM Clock
-    SYSCTL_RCC_R = SYSCTL_RCC_USESYSDIV | SYSCTL_RCC_PWMDIV_2;
+    SYSCTL_RCC_R |= 1;
+    SYSCTL_RCC_R |= SYSCTL_RCC_USESYSDIV;    //| SYSCTL_RCC_PWMDIV_2?
 
     //configuring
-//    GPIO_PORTG_DIR_R  = 0x04;
+    GPIO_PORTG_DIR_R  = 0x04;
     GPIO_PORTG_DEN_R  = 0x04;
     GPIO_PORTG_AFSEL_R = 0X04;
     GPIO_PORTG_PCTL_R = GPIO_PCTL_PG2_M1PWM0;
-//    GPIO_PORTG_AMSEL = 0X4;
+    GPIO_PORTG_AMSEL_R = 0X4;
 
     //configuring for countdown mode
     PWM0_CTL_R = 0x00000000;
@@ -25,7 +26,10 @@ int main(void)
     PWM0_0_LOAD_R = 0x7C; //for 80KHz time period=125 microseconds therefore 124
     PWM0_0_CMPA_R = 0x3E; //for 50% duty cycle
     PWM0_CTL_R = 0x00000001; //starting the timer in pwm0
-    PWM0_ENABLE_R = 0x00000003; //enable outputs
+    PWM0_ENABLE_R = 0x00000001; //enable outputs
 
-	return 0;
+    while (1)
+    {
+
+    }
 }
