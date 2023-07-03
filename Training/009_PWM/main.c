f6517b15dfb24733b02b3b4df6ab8d8b38bf53ec
Tapia Danish <danish.tapia588@gmail.com>
2023-07-04 00:43:57 +0530

    .

--- Training/009_PWM/main.c
+++ Training/009_PWM/main.c
@@ -1,33 +1,34 @@
-#include <tm4c123gh6pge.h>
 #include <stdint.h>
-//include 80mhz.h file
+#include <tm4c123gh6pge.h>
+#include <80mhz.h>
 
 
 int main(void)
 {
-    //call System80Mhz()
+    System80Mhz();
     //clock
-    SYSCTL_RCGC0_R |= SYSCTL_RCGC0_PWM0;            // PWM Clock
-    SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOG;
-    SYSCTL_RCC_R |= SYSCTL_RCC_USEPWMDIV;
-    SYSCTL_RCC_R &= (~0x0000E000);       // /2divider    clearing 3 bits for clock div
+    SYSCTL_RCGCPWM_R |= 0x2;
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R6; //SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOG;
 
-    //configuring
-    GPIO_PORTG_DIR_R = 0X04;
-    GPIO_PORTG_AFSEL_R = 0X04; //associated pin fns
-    GPIO_PORTG_DEN_R  = 0x04;
+    GPIO_PORTG_AFSEL_R = 0X04;
     GPIO_PORTG_PCTL_R = GPIO_PCTL_PG2_M1PWM0;
 
+
+    SYSCTL_RCC_R |= SYSCTL_RCC_USEPWMDIV;
+    SYSCTL_RCC_R &= (~0x000E0000);       // /2divider    clearing 3 bits for clock div
+
+
+
     //configuring for count down mode
-    PWM0_CTL_R = 0; //clearing
-    PWM0_0_GENA_R = PWM_0_GENA_ACTCMPAD_ZERO|PWM_0_GENA_ACTLOAD_M;
+    PWM1_0_CTL_R = 0; //clearing
+    PWM1_0_GENA_R = PWM_0_GENA_ACTCMPAD_ZERO|PWM_0_GENA_ACTLOAD_M;
 
     //generating
-    PWM0_0_LOAD_R = 1249;      //31.25*40 = 1250-1 1249
-    PWM0_0_CMPA_R = 625;      //for 50% duty cycle
-    PWM0_CTL_R |= PWM_0_CTL_ENABLE|PWM_0_CTL_MODE;           //starting the timer in pwm0
-    PWM0_ENABLE_R |= PWM_ENABLE_PWM0EN;        //enable outputs
-]
+    PWM1_0_LOAD_R = 1249;       //31.25*40 = 1250-1 1249
+    PWM1_0_CMPA_R = 625;        //for 50% duty cycle
+    PWM1_0_CTL_R |= PWM_0_CTL_ENABLE;
+    PWM1_ENABLE_R |= PWM_ENABLE_PWM0EN;        //enable outputs
+
     //for 32KHz frequency, time period = 31.25 microseconds
     //PWM clock is half of system clock = 40 Mhz
 
