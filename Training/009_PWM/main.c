5728b9cf1e8b447e69192296f6913794a268db16
Tapia Danish <danish.tapia588@gmail.com>
2023-07-04 09:44:21 +0530

    working code

--- Training/009_PWM/main.c
+++ Training/009_PWM/main.c
@@ -19,20 +19,20 @@ int main(void)
     GPIO_PORTG_PCTL_R |= GPIO_PCTL_PG2_M1PWM0;
 
 
-//    SYSCTL_RCC_R |= SYSCTL_RCC_USEPWMDIV;
-//    SYSCTL_RCC_R &= (~0x000E0000);       // /2divider    clearing 3 bits for clock div
+    SYSCTL_RCC_R |= SYSCTL_RCC_USEPWMDIV;
+    SYSCTL_RCC_R &= (~0x000E0000);       // /2divider    clearing 3 bits for clock div
 
 
 
     //configuring for count down mode
-    PWM1_0_CTL_R = 0; //clearing
-    PWM1_0_GENA_R |= PWM_0_GENA_ACTCMPAD_ZERO|PWM_0_GENA_ACTLOAD_M;
+    PWM1_0_CTL_R &= (~1); //clearing
+    PWM1_0_GENA_R = 0x8C; //PWM_0_GENA_ACTCMPAD_ZERO|PWM_0_GENA_ACTLOAD_M
 
     //generating
-    PWM1_0_LOAD_R = 1249;       //31.25*40 = 1250-1 1249
+    PWM1_0_LOAD_R = 1250;       //31.25*40 = 1250-1 1249
     PWM1_0_CMPA_R = 625;        //for 50% duty cycle
-    PWM1_0_CTL_R |= PWM_0_CTL_ENABLE;
-    PWM1_ENABLE_R |= PWM_ENABLE_PWM0EN;        //enable outputs
+    PWM1_0_CTL_R |= 0x1;        //PWM_0_CTL_ENABLE
+    PWM1_ENABLE_R |= 0x1;        //PWM_ENABLE_PWM0EN
 
     //for 32KHz frequency, time period = 31.25 microseconds
     //PWM clock is half of system clock = 40 Mhz
